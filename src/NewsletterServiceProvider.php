d0341098d053d6f68c4ae9285cf8c4e2fb394dc6
Francisco Sepulveda <socieboy@gmail.com>
2015-05-28 19:33:55 -0700

    Initialize

---
+++ src/NewsletterServiceProvider.php
@@ -0,0 +1,27 @@
+<?php namespace Socieboy\Newsletter;
+
+use Illuminate\Support\ServiceProvider;
+
+class NewsletterServiceProvider extends ServiceProvider {
+
+	/**
+	 * Bootstrap the application services.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		//
+	}
+
+	/**
+	 * Register the application services.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+}
